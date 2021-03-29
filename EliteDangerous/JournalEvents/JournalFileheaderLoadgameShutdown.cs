5a4ad7ca6dc26fd0304af0b0411e32c1865209ee
robbyxp1 <robxp@hotmail.com>
2021-03-29 13:51:13 +0100

    Add Alpha check for Odyssey

--- EliteDangerous/JournalEvents/JournalFileheaderLoadgameShutdown.cs
+++ EliteDangerous/JournalEvents/JournalFileheaderLoadgameShutdown.cs
@@ -41,7 +41,7 @@ namespace EliteDangerousCore.JournalEvents
         {
             get
             {
-                if (GameVersion.Contains("Beta") )
+                if (GameVersion.Contains("Beta") || GameVersion.Contains("Alpha"))
                     return true;
 
                 if (GameVersion.Contains("April Update EDH") && ( Build.Contains("r198057/r0") || Build.Contains("r197746/r0")))
