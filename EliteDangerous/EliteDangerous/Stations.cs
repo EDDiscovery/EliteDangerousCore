dab59af9830aca0133910a6593ef34a68185f6e4
robbyxp1 <robxp@hotmail.com>
2025-03-03 22:25:51 +0000

    Add construction to startport state

--- EliteDangerous/EliteDangerous/Stations.cs
+++ EliteDangerous/EliteDangerous/Stations.cs
@@ -239,6 +239,7 @@ namespace EliteDangerousCore
             Damaged,
             Abandoned,
             UnderAttack,
+            Construction,
         }
 
         // maps the StationType field to an enum
